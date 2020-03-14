e614bfe3ab3ac3e9acd1956e1499a9377d1fe510
Unai Gonzalez <unaigoe91@gmail.com>
2020-03-14 16:54:33 +0100

    BCN-FT-Unai Gonzalez

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("C:\\Users\\PCCOM MSI\\IRONHACK\\lab-express-cinema\\starter_code\\models\\models");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req,res,next) => {
+  
+  Movie.find()
+  .then(allMoviesFromDB => {
+  //console.log(`Movies from Db ${allMoviesFromDB}`)
+    res.render('movies', {allMoviesFromDB})
+  })
+  .catch(error => {
+    //console.log('Error while getting movies from the DB: ', error);
+  });
+})
+
+router.get('/movie/:id', (req,res,next) => {
+  //console.log(req.params.id)
+  Movie.findOne({'_id': req.params.id})
+  .then(oneMovie => {
+    //console.log(oneMovie)
+    
+    res.render('movie-features', {movies: oneMovie});
+  })
+  
+  .catch(err => console.log(`You made a mistake ${err} `))
+  
+})
+
 
 module.exports = router;
